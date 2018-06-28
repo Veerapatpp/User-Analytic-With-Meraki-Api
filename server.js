63afeefdcbba1369537199b281dba84765bdf168
ppomio <ppomiovrp@gmail.com>
2018-06-28 12:54:07 +0700

    slaaa

--- server.js
+++ server.js
@@ -28,21 +28,22 @@ console.log("sending validation")
 
 
 app.post('/meraki', function(req, res){
-try {
-var jsoned = JSON.parse(req.body.data);
-if (jsoned.secret == secret) {
-for (i=0; i<jsoned.probing.length; i++) {
-console.log("client " + jsoned.probing[i].client_mac + " seen on ap " + jsoned.probing[i].ap_mac + " with rssi " + jsoned.probing[i].rssi + " at " + jsoned.probing[i].last_seen);
-}
-} else {
-console.log("invalid secret from " + req.connection.remoteAddress);
-}
-} catch (e) {
-// An error has occured, handle it, by e.g. logging it
-console.log("Error. Likely caused by an invalid POST from " + req.connection.remoteAddress + ":");
-console.log(e);
+    console.log(req.body)
+// try {
+// var jsoned = JSON.parse(req.body.data);
+// if (jsoned.secret == secret) {
+// for (i=0; i<jsoned.probing.length; i++) {
+// console.log("client " + jsoned.probing[i].client_mac + " seen on ap " + jsoned.probing[i].ap_mac + " with rssi " + jsoned.probing[i].rssi + " at " + jsoned.probing[i].last_seen);
+// }
+// } else {
+// console.log("invalid secret from " + req.connection.remoteAddress);
+// }
+// } catch (e) {
+// // An error has occured, handle it, by e.g. logging it
+// console.log("Error. Likely caused by an invalid POST from " + req.connection.remoteAddress + ":");
+// console.log(e);
 res.end();
-}
+//}
 
 });
 
